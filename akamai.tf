e03e44736b8c102bed151e337ef544315c31b0e8
elfinston <elfinston@gmail.com>
2021-12-12 20:33:26 +0900

    Update akamai.tf

--- akamai.tf
+++ akamai.tf
@@ -66,7 +66,7 @@ resource "akamai_property_activation" "staging-activation" {
 # Activation Production
 resource "akamai_property_activation" "production-activation" {
   property_id = akamai_property.default.id
-  version = 3
+  version = 4
   network = "PRODUCTION"
   contact = ["shki@akamai.com"]
 }
